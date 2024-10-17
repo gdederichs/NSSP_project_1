a6e7401dc9b55305bc2f0981c4edd6ad4c7fa66e
Gonçalo Nascimento Vasconcelos Braga <gnascime@sti-nx421-005.intranet.epfl.ch>
2024-10-17 17:23:19 +0200

    bring everything to same point

---
+++ utils.py
@@ -0,0 +1,19 @@
+class DownloadProgressBar(tqdm):
+    def update_to(self, b=1, bsize=1, tsize=None):
+        if tsize is not None:
+            self.total = tsize
+        self.update(b * bsize - self.n)
+
+def download_url(url, output_path):
+    with DownloadProgressBar(unit='B', unit_scale=True,
+                             miniters=1, desc=url.split('/')[-1]) as t:
+        urllib.request.urlretrieve(url, filename=output_path, reporthook=t.update_to)
+
+def direct_file_download_open_neuro(file_list, file_types, dataset_id, dataset_version, save_dirs):
+    # https://openneuro.org/crn/datasets/ds004226/snapshots/1.0.0/files/sub-001:sub-001_scans.tsv
+    for i, n in enumerate(file_list):
+        subject = n.split('_')[0]
+        download_link = 'https://openneuro.org/crn/datasets/{}/snapshots/{}/files/{}:{}:{}'.format(dataset_id, dataset_version, subject, file_types[i],n)
+        print('Attempting download from ', download_link)
+        download_url(download_link, op.join(save_dirs[i], n))
+        print('Ok')
