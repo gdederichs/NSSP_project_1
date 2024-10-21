863a2918d3494c9478be779471b280765985f4bf
Neurodesk User <user@neurodesk.org>
2024-10-21 10:56:25 +0000

    new files for design matrix

--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
-class DownloadProgressBar(tqdm):
-    def update_to(self, b=1, bsize=1, tsize=None):
-        if tsize is not None:
-            self.total = tsize
-        self.update(b * bsize - self.n)
+# class DownloadProgressBar(tqdm):
+#     def update_to(self, b=1, bsize=1, tsize=None):
+#         if tsize is not None:
+#             self.total = tsize
+#         self.update(b * bsize - self.n)
 
 def download_url(url, output_path):
     with DownloadProgressBar(unit='B', unit_scale=True,
@@ -17,3 +17,18 @@ def direct_file_download_open_neuro(file_list, file_types, dataset_id, dataset_v
         print('Attempting download from ', download_link)
         download_url(download_link, op.join(save_dirs[i], n))
         print('Ok')
+
+
+def mkdir_no_exist(path):
+    """
+    Function to create a directory if it does not exist already.
+
+    Parameters
+    ----------
+    path: string
+        Path to create
+    """
+    import os
+    import os.path as op
+    if not op.isdir(path):
+        os.makedirs(path)
